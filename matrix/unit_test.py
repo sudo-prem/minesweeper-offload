4096964e21627795475915c908abf5e0290860c6
premnaaath <premnaathvaradharajan@gmail.com>
2022-12-06 22:05:33 +0530

    Update MM UT

--- matrix/unit_test.py
+++ matrix/unit_test.py
@@ -5,30 +5,32 @@ from mul import *
 
 class TestMatrixMultiplication(unittest.TestCase):
     def test_matrix_multiplication(self):
-        n = 2
-        random.seed(1234)
-        matmul = MatrixMultiplication(n)
-        res = matmul.standard_matrix_product()
-        create_mat(n)
-        # open the file
-        with open("matrix.out") as f:
-            lines = f.read().splitlines()
 
-        self.A = []
-        self.B = []
-        self.matrix = self.A
-        for line in lines:
-            if line != "":
-                self.matrix.append([int(el) for el in line.split("\t")])
-            else:
-                self.matrix = self.B
+        for i in range(1, 10):
+            n = i
+            random.seed(1234)
+            matmul = MatrixMultiplication(n)
+            res = matmul.standard_matrix_product()
+            create_mat(n)
+            # open the file
+            with open("matrix.out") as f:
+                lines = f.read().splitlines()
 
-        C = [[0 for i in xrange(n)] for j in xrange(n)]
-        for i in xrange(n):
-            for j in xrange(n):
-                for k in xrange(n):
-                    C[i][j] += self.A[i][k] * self.B[k][j]
-        self.assertEqual(res, C)
+            self.A = []
+            self.B = []
+            self.matrix = self.A
+            for line in lines:
+                if line != "":
+                    self.matrix.append([int(el) for el in line.split("\t")])
+                else:
+                    self.matrix = self.B
+
+            C = [[0 for i in xrange(n)] for j in xrange(n)]
+            for i in xrange(n):
+                for j in xrange(n):
+                    for k in xrange(n):
+                        C[i][j] += self.A[i][k] * self.B[k][j]
+            self.assertEqual(res, C)
 
 
 if __name__ == "__main__":
